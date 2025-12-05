d9c641ad050c974f9e9718a3ab197edf0c757244
YAadnane <yadani.adnane20@gmail.com>
2025-12-05 20:55:19 +0100

    fix: remove leading whitespace from sitemap.xml to prevent parsing errors

--- server/index.js
+++ server/index.js
@@ -486,39 +486,40 @@ app.get('/sitemap.xml', (req, res) => {
     const baseUrl = 'https://yadani-adnane.duckdns.org';
     const lastMod = new Date().toISOString().split('T')[0];
     
+    // NOTE: The <?xml declaration MUST be the very first characters. No whitespace allowed.
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>${baseUrl}/</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>1.0</priority>
-        </url>
-        <url>
-            <loc>${baseUrl}/#about</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>0.8</priority>
-        </url>
-        <url>
-            <loc>${baseUrl}/#skills</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>0.8</priority>
-        </url>
-        <url>
-            <loc>${baseUrl}/#work</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>0.8</priority>
-        </url>
-        <url>
-            <loc>${baseUrl}/#education</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>0.7</priority>
-        </url>
-        <url>
-            <loc>${baseUrl}/#contact</loc>
-            <lastmod>${lastMod}</lastmod>
-            <priority>0.7</priority>
-        </url>
-    </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${baseUrl}/</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/#about</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/#skills</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/#work</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/#education</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/#contact</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>0.7</priority>
+    </url>
+</urlset>`;
     
     res.header('Content-Type', 'application/xml');
     res.send(xml);
