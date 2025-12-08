a3254c246efc0c9c28afd1a09008bb3119ae3d9a
YAadnane <yadani.adnane20@gmail.com>
2025-12-08 22:59:39 +0100

    fix: correct database path in migration script

--- server/migrate_i18n.cjs
+++ server/migrate_i18n.cjs
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../portfolio.db');
+const dbPath = path.resolve(__dirname, 'portfolio.db');
 const db = new sqlite3.Database(dbPath);
 
 const tables = [
