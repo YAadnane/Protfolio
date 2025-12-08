0b75c3d40f7439692ae4046f97ab8f8c52f5e486
YAadnane <yadani.adnane20@gmail.com>
2025-12-08 22:50:18 +0100

    fix: remove duplicate code in migration script

--- server/migrate_i18n.cjs
+++ server/migrate_i18n.cjs
@@ -1,15 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../portfolio.db');
-const db = new sqlite3.Database(dbPath);
-
-const tables = [
-    'general_info',
-    'projects',
-    'education',
-    'experience',
-    'certifications',
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
